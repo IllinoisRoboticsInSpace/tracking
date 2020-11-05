56301c2cd0336042f1ea7bd87e15e449eb8391c2
Ananya Y <pv@pine-cliff.us-central1-a.c.proud-lamp-288416.internal>
2020-11-05 22:13:50 +0000

    Make big map template

---
+++ catkin_ws/src/iris-main/main_map.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+
+local_map = np.array((insert_dimensions_here))
+
+def placeholder():
+    # set up subscribers to:
+    # - imu
+    # - jetson
+    # - realsense
+    # - Any other sensors (none as of now)
+
+    # update local map data structure as sensor data determines
+    pass
+
+def process_sensor_input(): # rename this function and write it
+
+if __name__ == '__main__':
+    try:
+        placeholder()
+    except rospy.ROSInterruptException:
+      

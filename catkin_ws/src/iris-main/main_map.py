ebd6a2294ac3e6f9e2c12489748d8b159b149bd0
Chris C <coronadocx@gmail.com>
2020-11-05 19:07:43 -0600

    Need to figure out how to publish IMU data w Bhargav

--- catkin_ws/src/iris-main/main_map.py
+++ catkin_ws/src/iris-main/main_map.py
@@ -4,7 +4,27 @@ import numpy as np
 
 local_map = np.array((insert_dimensions_here))
 
-def placeholder():
+# def callback_imu(data):
+    # rospy.loginfo(rospy.get_caller_id() + " received data from imu %s", data.range)
+    # Will be publishing robot displacement, some form of deltaX deltaY
+    # This will be used to update the system
+    # For now, assume this won't be publishing anything
+    # TODO
+
+# def callback_jetson(data):
+    # rospy.loginfo(rospy.get_caller_id() + " received data from jetson %s", data.range)
+    # IF this is running a NN, might be publishing a list of obstacles.
+    # For now, assume this won't be publishing anything
+    # TODO
+
+def callback_realSense(data):
+    rospy.loginfo(rospy.get_caller_id() + " received data from realSense %s", data.range)
+    # Has a ROS interface, and use this fordata purposes
+    # Probably where we're going to get the data from
+    # TODO
+
+
+def main_map_listener():
     # set up subscribers to:
     # - imu
     # - jetson
@@ -15,6 +35,9 @@ def placeholder():
     # pass
     
     rospy.init_node('main_map')
+    # rospy.Subscriber("<TOPIC>", <DATA_TYPE>, callback_imu)        # Will probably be removed
+    # rospy.Subscriber("<TOPIC>", <DATA_TYPE>, callback_jetson)     # Will probably be removed 
+    rospy.Subscriber("/camera/accel/imu_info", <DATA_TYPE>, callback_realSense)     # Need to setup realsense node before we know the topic name 
 
     # TODO
 
@@ -24,6 +47,6 @@ def process_sensor_input(): # rename this function and write it
 
 if __name__ == '__main__':
     try:
-        placeholder()
+        main_map_listener()
     except rospy.ROSInterruptException:
       
